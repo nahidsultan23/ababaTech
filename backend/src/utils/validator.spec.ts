7213c89007a7c836f6f2f390a738c81e939ec6a7
Nahid Sultan <n.sultan@amagroup.io>
2022-08-29 17:43:41 +0600

    More unit tests added to the backend

--- backend/src/utils/validator.spec.ts
+++ backend/src/utils/validator.spec.ts
@@ -1,4 +1,4 @@
-import { isValidEmail } from './validator';
+import { comparePasswords, isMongoDbObjectId, isValidEmail } from './validator';
 
 describe('test isValidEmail', () => {
   it('should return error for no @ and .', async () => {
@@ -19,5 +19,35 @@ describe('test isValidEmail', () => {
   it('should not return error for proper email address', async () => {
     const testEmail = isValidEmail('nahid@ababatech.com');
     expect(testEmail).not.toBe(null);
+  });
+});
+
+describe('test comparePasswords', () => {
+  it('should return false for non-matching passwords', async () => {
+    const testPasswordComparison = await comparePasswords(
+      '123987',
+      '$2a$10$WFuS5uT4O9lzGPlF4NhOzOBnyCnyYwXMmY3TnNv/xdgKCHm0JkXR6',
+    );
+    expect(testPasswordComparison).toBe(false);
+  });
+
+  it('should return true for matching passwords', async () => {
+    const testPasswordComparison = await comparePasswords(
+      '123456',
+      '$2a$10$WFuS5uT4O9lzGPlF4NhOzOBnyCnyYwXMmY3TnNv/xdgKCHm0JkXR6',
+    );
+    expect(testPasswordComparison).toBe(true);
+  });
+});
+
+describe('test isMongoDbObjectId', () => {
+  it('should return false for non-mongodb objectId string', async () => {
+    const testMongoDbObjectId = isMongoDbObjectId('630x6693721f7fd721f9a8b0');
+    expect(testMongoDbObjectId).toBe(false);
+  });
+
+  it('should return true for mongodb objectId string', async () => {
+    const testMongoDbObjectId = isMongoDbObjectId('63006693721f7fd721f9a8b0');
+    expect(testMongoDbObjectId).toBe(true);
   });
 });
